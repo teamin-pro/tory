8380f0988f167549879309f686dd08848b79795e
smee <maxim.oransky@gmail.com>
2023-11-02 18:39:54 +0100

    Select[T]() return nullable types

--- queries.go
+++ queries.go
@@ -47,13 +47,13 @@ func QueryRow(db Tory, name string, args Args, fields ...any) error {
 	return nil
 }
 
-func Select[T any](db Tory, name string, args Args) ([]T, error) {
+func Select[T any](db Tory, name string, args Args) ([]*T, error) {
 	query, err := db.Query(name)
 	if err != nil {
 		return nil, err
 	}
 
-	result := make([]T, 0)
+	result := make([]*T, 0)
 	if err := pgxscan.Select(context.Background(), db.pool, &result, query.Body(), query.Args(args)...); err != nil {
 		return nil, errors.Wrapf(err, "select fail on `%s`", name)
 	}
