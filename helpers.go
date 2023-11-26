ed9c4371572b5ce12f63b89be2b106c0058e816d
smee <maxim.oransky@gmail.com>
2023-11-27 00:35:24 +0100

    IsDuplicateKeyValueViolatesUniqueConstraint helper

--- helpers.go
+++ helpers.go
@@ -1,6 +1,11 @@
 package tory
 
-import "strings"
+import (
+	"strings"
+
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/pkg/errors"
+)
 
 var likeEscape = strings.NewReplacer(
 	"%", "\\%",
@@ -11,4 +16,21 @@ var likeEscape = strings.NewReplacer(
 
 func LikeEscape(s string) string {
 	return likeEscape.Replace(s)
+}
+
+func IsDuplicateKeyValueViolatesUniqueConstraint(err error) bool { return isPgError(err, "23505") }
+
+func IsViolationOfCheckConstraint(err error) bool { return isPgError(err, "23514") }
+
+func isPgError(err error, code string) bool {
+	if err == nil {
+		return false
+	}
+
+	var pgErr *pgconn.PgError
+	if errors.As(err, &pgErr) {
+		return pgErr.Code == code
+	}
+
+	return false
 }
